8568495021fc2cc90a6399b2e3eef16b8e0040a7
Brooklynn Hathaway <hearthebrook@gmail.com>
2024-04-24 16:08:07 -0600

    started home page

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+
+
+let year = new Date().toLocaleDateString("en-US", {year:"numeric"});
+// document.querySelector("p").innerHTML = year
+
+
+document.querySelector("#lastModified").innerHTML = new Date(document.lastModified);
+
+
+
+
+
