9f18ec069518811381e6c50c40c2438de082fde1
Brooklynn Hathaway <hearthebrook@gmail.com>
2024-04-24 16:40:20 -0600

    fixed year in footer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,7 @@
 
 
 let year = new Date().toLocaleDateString("en-US", {year:"numeric"});
-// document.querySelector("p").innerHTML = year
+document.querySelector("#year").innerHTML = year
 
 
 document.querySelector("#lastModified").innerHTML = new Date(document.lastModified);
